60482e1d7f0270f05e9fdfac1b7d4b3f9636a47e
FabioRieker <faabiioo05@gmail.com>
2026-02-04 12:39:46 +0100

    hola

--- proyectoBingo/src/prueba/Prueba.java
+++ proyectoBingo/src/prueba/Prueba.java
@@ -5,6 +5,10 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("hola");
+			System.out.println("hehe");
+			System.out.println("huhuhu");
+			System.out.println("hhuuhhuuhhuuhhuuhhuuhhuuhuhu");
+		
 	}
 
 }
